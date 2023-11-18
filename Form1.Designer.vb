709c403f647435fb4f80ace84ae63927c23f2125
olamide3412 <48572410+olamide3412@users.noreply.github.com>
2023-11-18 15:40:04 +0100

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,126 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnDecode = New System.Windows.Forms.Button()
+        Me.btnEncode = New System.Windows.Forms.Button()
+        Me.txtEncode = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtDecode = New System.Windows.Forms.TextBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnDecode
+        '
+        Me.btnDecode.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDecode.Location = New System.Drawing.Point(326, 223)
+        Me.btnDecode.Name = "btnDecode"
+        Me.btnDecode.Size = New System.Drawing.Size(75, 30)
+        Me.btnDecode.TabIndex = 0
+        Me.btnDecode.Text = "Decode"
+        Me.btnDecode.UseVisualStyleBackColor = True
+        '
+        'btnEncode
+        '
+        Me.btnEncode.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEncode.Location = New System.Drawing.Point(407, 223)
+        Me.btnEncode.Name = "btnEncode"
+        Me.btnEncode.Size = New System.Drawing.Size(75, 30)
+        Me.btnEncode.TabIndex = 1
+        Me.btnEncode.Text = "Encode"
+        Me.btnEncode.UseVisualStyleBackColor = True
+        '
+        'txtEncode
+        '
+        Me.txtEncode.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtEncode.Location = New System.Drawing.Point(138, 151)
+        Me.txtEncode.Name = "txtEncode"
+        Me.txtEncode.Size = New System.Drawing.Size(344, 30)
+        Me.txtEncode.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 154)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(90, 25)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Encode :"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(470, 133)
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 190)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(91, 25)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Decode :"
+        '
+        'txtDecode
+        '
+        Me.txtDecode.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtDecode.Location = New System.Drawing.Point(138, 187)
+        Me.txtDecode.Name = "txtDecode"
+        Me.txtDecode.Size = New System.Drawing.Size(344, 30)
+        Me.txtDecode.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(491, 277)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtDecode)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtEncode)
+        Me.Controls.Add(Me.btnEncode)
+        Me.Controls.Add(Me.btnDecode)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnDecode As Button
+    Friend WithEvents btnEncode As Button
+    Friend WithEvents txtEncode As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtDecode As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,18 @@
+﻿Imports ZXing
+
+Public Class Form1
+    Private Sub btnEncode_Click(sender As Object, e As EventArgs) Handles btnEncode.Click
+        Dim writer As New BarcodeWriter
+        writer.Format = BarcodeFormat.CODE_128
+        PictureBox1.Image = writer.Write(txtEncode.Text)
+
+    End Sub
+
+    Private Sub btnDecode_Click(sender As Object, e As EventArgs) Handles btnDecode.Click
+        Dim reader As New BarcodeReader
+        Dim result = reader.Decode(CType(PictureBox1.Image, Bitmap))
+        If result IsNot Nothing Then
+            txtDecode.Text = result.Text
+        End If
+    End Sub
+End Class
